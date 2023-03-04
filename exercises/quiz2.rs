b7f70e3038980805e0014101b9b92ca53d8aeb53
HRusby <harrygeorger@hotmail.com>
2023-03-04 15:34:10 +0000

    Next batch of exercises

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,7 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -27,14 +26,29 @@ pub enum Command {
 }
 
 mod my_module {
+
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(sz) => {
+                    let mut amended_str = string.to_string();
+                    // Two Methods  - As Iterator Foreach
+                    (0..*sz).into_iter().for_each(|x| amended_str += "bar");
+                    //              - As For Loop
+                    // for _ in 0..*sz {
+                    //    amended_str += "bar" 
+                    // }
+                    output.push(amended_str);
+                }
+            }
         }
         output
     }
@@ -43,7 +57,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
